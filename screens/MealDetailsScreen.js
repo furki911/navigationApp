e477aa3ab6fdf5c7fab5361bbe918b255ec51b17
Furqan Masood <bhatfurqan@gmail.com>
2024-05-16 00:56:35 +0530

    MealDetails Screen Added - III Completed All

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,35 +1,72 @@
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/MealDetail/Subtitle";
+import List from "../components/MealDetail/List";
 
 const MealDetailsScreen = (props) => {
   const { route } = props;
   const mealId = route.params.mealId;
   const selectedMeal = MEALS.find((item) => item.id === mealId);
-  const { duration, complexity, affordability, imageUrl, ingredients, steps } =
-    selectedMeal;
+
+  const {
+    title,
+    duration,
+    complexity,
+    affordability,
+    imageUrl,
+    ingredients,
+    steps,
+  } = selectedMeal;
 
   return (
-    <View>
-      <Image source={{ uri: imageUrl }} />
-      <Text>title</Text>
+    <ScrollView style={styles.rootContainer}>
+      <Image style={styles.image} source={{ uri: imageUrl }} />
+      <Text style={styles.title}>{title}</Text>
 
       <MealDetails
         duration={duration}
         complexity={complexity}
         affordability={affordability}
+        textStyle={styles.detailText}
       />
-      <Text>Ingredients</Text>
-      {ingredients.map((ingredient) => (
-        <Text key={ingredient}>{ingredient}</Text>
-      ))}
-      <Text>Steps</Text>
-      {steps.map((step) => (
-        <Text key={step}>{step}</Text>
-      ))}
-    </View>
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={ingredients} />
+          <Subtitle>Subtitle</Subtitle>
+          <List data={steps} />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 32,
+  },
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "#ffffff",
+  },
+  detailText: {
+    color: "#ffffff",
+  },
+  listOuterContainer: {
+    alignItems: "center",
+  },
+  listContainer: {
+    width: "80%",
+  },
+});
 
 export default MealDetailsScreen;
